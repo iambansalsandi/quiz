1352b942109cb7cd2c4a9ebcd2d75961cde24ae6
iambansalsandi <isandeepbansal4o4@gmail.com>
2019-03-31 13:19:24 +0530

    Basic bootstrap structure for app

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Question box</h1>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template slot="lead">Some question goes here..</template>
+
+      <hr class="my-4">
+
+      <p>List of all answer</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
